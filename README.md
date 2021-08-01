6afdf90d2489906828514faeeae4e6c7fc3df21c
Igor Faoro <igor.faoro17@gmail.com>
2021-08-01 09:57:34 -0300

    devices list2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Seigas Repy Panel
+# Repy Panel
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/seigas-repy-panel'),
+      dir: require('path').join(__dirname, './coverage/seig-repy-panel'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
